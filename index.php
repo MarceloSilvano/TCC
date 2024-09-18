c6116b47bbdca7cd14a6ebacda7d41a70dfc7e65
MarceloSilvano <marcelocgsilvano@gmail.com>
2024-09-18 18:55:52 -0300

    nav separada

--- index.php
+++ index.php
@@ -9,27 +9,8 @@
 </head>
 <body>
     <div class="container-md">
-        <nav class="navbar navbar-expand-sm bg-white navbar-light">
-            <div class="container-fluid">
-            <a class="navbar-brand" href="#">AcadRepo</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavbar">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="collapsibleNavbar">
-                <ul class="navbar-nav">
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Login</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Sobre</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Contato</a>
-                </li>
-                </ul>
-            </div>
-            </div>
-        </nav>
+        
+        <?php include_once("./page-parts/nav.php") ?>
 
         <div class="container-sm">
 
---
+++ page-parts/nav.php
@@ -0,0 +1,21 @@
+<nav class="navbar navbar-expand-sm bg-white navbar-light">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="#">AcadRepo</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavbar">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="collapsibleNavbar">
+            <ul class="navbar-nav">
+            <li class="nav-item">
+                <a class="nav-link" href="#">Login</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Sobre</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Contato</a>
+            </li>
+            </ul>
+        </div>
+    </div>
+</nav>
